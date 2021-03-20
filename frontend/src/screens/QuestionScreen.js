d67c82123340e6a331fc610bf7a8989a15780593
Codefreak69 <iamvikashkumar30@gmail.com>
2021-03-21 03:11:34 +0530

    show button

--- frontend/src/screens/QuestionScreen.js
+++ frontend/src/screens/QuestionScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button'
 import { useDispatch, useSelector } from "react-redux";
 import {
     Image,
@@ -28,7 +29,14 @@ const QuestionScreen = ({ history, match }) => {
     }, [dispatch, match]);
     function ControlledTabs() {
         const [key, setKey] = useState("prompt");
-
+    function showSolution(){
+        var x = document.getElementById("solution")
+        if(x.style.display=="none"){
+            x.style.display = "block"
+        }else{
+            x.style.display = "none"
+        }
+    }
         return (
             <Tabs
                 id="Difficulty Control"
@@ -44,7 +52,9 @@ const QuestionScreen = ({ history, match }) => {
                     <h3>{question.hints}</h3>
                 </Tab>
                 <Tab eventKey="sollution" title="Sollution">
-                    <h3>{question.sollution}</h3>
+                    <div id="solution">
+                    {question.sollution}
+                    <Button onClick="showSolution()"variant="outline-info">Click to view solution</Button>{' '}</div>
                 </Tab>
             </Tabs>
         );
