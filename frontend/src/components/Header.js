ad8d52b2f1c294907a84afd0ad31299d142df44d
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-03-20 18:56:01 +0530

    changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
-import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 
 const Header = () => {
@@ -25,7 +24,6 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className='ml-auto'>
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
--- frontend/src/screens/QuestionScreen.js
+++ frontend/src/screens/QuestionScreen.js
@@ -1,157 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import Meta from '../components/Meta'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  listQuestionDetails,
-  createQuestionReview,
-} from '../actions/questionActions'
-import { QUESTION_CREATE_REVIEW_RESET } from '../constants/questionConstants'
+    Row,
+    Col,
+    Image,
+    ListGroup,
+    Card,
+    Button,
+    Form,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import Meta from "../components/Meta";
+import {
+    listQuestionDetails,
+    createQuestionReview,
+} from "../actions/questionActions";
+import { QUESTION_CREATE_REVIEW_RESET } from "../constants/questionConstants";
 
 const QuestionScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(1)
-  const [rating, setRating] = useState(0)
-  const [comment, setComment] = useState('')
+    const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+    const questionDetails = useSelector((state) => state.questionDetails);
+    const { loading, error, question } = questionDetails;
 
-  const questionDetails = useSelector((state) => state.questionDetails)
-  const { loading, error, question } = questionDetails
+    useEffect(() => {
+        if (!question._id || question._id !== match.params.id) {
+            dispatch(listQuestionDetails(match.params.id));
+        }
+    }, [dispatch, match]);
 
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
-
-  const questionReviewCreate = useSelector((state) => state.questionReviewCreate)
-  const {
-    success: successQuestionReview,
-    loading: loadingQuestionReview,
-    error: errorQuestionReview,
-  } = questionReviewCreate
-
-  useEffect(() => {
-    if (successQuestionReview) {
-      setRating(0)
-      setComment('')
-    }
-    if (!question._id || question._id !== match.params.id) {
-      dispatch(listQuestionDetails(match.params.id))
-      dispatch({ type: QUESTION_CREATE_REVIEW_RESET })
-    }
-  }, [dispatch, match, successQuestionReview])
-
-  const addToCartHandler = () => {
-    history.push(`/cart/${match.params.id}?qty=${qty}`)
-  }
-
-  const submitHandler = (e) => {
-    e.preventDefault()
-    dispatch(
-      createQuestionReview(match.params.id, {
-        rating,
-        comment,
-      })
-    )
-  }
-
-  return (
-    <>
-      <Link className='btn btn-light my-3' to='/'>
-        Go Back
-      </Link>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant='danger'>{error}</Message>
-      ) : (
+    return (
         <>
-          <Meta title={question.name} />
-          <Row>
-            <Col md={6}>
-              <Image src={question.image} alt={question.name} fluid />
-            </Col>
-            <Col md={3}>
-              <ListGroup variant='flush'>
-                <ListGroup.Item>
-                  <h3>{question.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating
-                    value={question.rating}
-                    text={`${question.numReviews} reviews`}
-                  />
-                </ListGroup.Item>
-                <ListGroup.Item>Price: ${question.price}</ListGroup.Item>
-                <ListGroup.Item>
-                  Description: {question.description}
-                </ListGroup.Item>
-              </ListGroup>
-            </Col>
-            <Col md={3}>
-              <Card>
-                <ListGroup variant='flush'>
-                  <ListGroup.Item>
+            <Link className="btn btn-light my-3" to="/">
+                Go Back
+            </Link>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <>
+                    <Meta title={question.name} />
                     <Row>
-                      <Col>Price:</Col>
-                      <Col>
-                        <strong>${question.price}</strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
-                        {question.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-
-                  {question.countInStock > 0 && (
-                    <ListGroup.Item>
-                      <Row>
-                        <Col>Qty</Col>
-                        <Col>
-                          <Form.Control
-                            as='select'
-                            value={qty}
-                            onChange={(e) => setQty(e.target.value)}
-                          >
-                            {[...Array(question.countInStock).keys()].map(
-                              (x) => (
-                                <option key={x + 1} value={x + 1}>
-                                  {x + 1}
-                                </option>
-                              )
-                            )}
-                          </Form.Control>
+                        <Col md={6}>
+                            <Image
+                                src={question.image}
+                                alt={question.name}
+                                fluid
+                            />
                         </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  )}
-
-                  <ListGroup.Item>
-                    <Button
-                      onClick={addToCartHandler}
-                      className='btn-block'
-                      type='button'
-                      disabled={question.countInStock === 0}
-                    >
-                      Add To Cart
-                    </Button>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card>
-            </Col>
-          </Row>
+                        <Col md={3}>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <h3>{question.name}</h3>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Description: {question.description}
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    </Row>
+                </>
+            )}
         </>
-      )}
-    </>
-  )
-}
+    );
+};
 
-export default QuestionScreen
+export default QuestionScreen;
