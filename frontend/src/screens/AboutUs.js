84bd800f45dab30fa603ee1079b241dd82b7db6a
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-03-21 06:23:57 +0530

    About Us

--- frontend/src/screens/AboutUs.js
+++ frontend/src/screens/AboutUs.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Container, Image } from "react-bootstrap";
+import { Container,Card, Image,Row, Col, Button } from "react-bootstrap";
 import Meta from "../components/Meta";
-import Button from "react-bootstrap/Button";
 import { listQuestions } from "../actions/questionActions";
 
 const AboutUs = ({ match }) => {
@@ -11,53 +10,87 @@ const AboutUs = ({ match }) => {
     <>
       <Meta />
       <Container>
-      <div class="about-section">
+          <Container>
   <h1>About Us Page</h1>
   <p>Some text about who we are and what we do.</p>
   <p>Resize the browser window to see that this page is responsive by the way.</p>
-</div>
+          </Container>
 
-<h2 style="text-align:center">Our Team</h2>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Jane Doe</h2>
-        <p class="title">CEO & Founder</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>jane@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
+          <center><h2>Our Team</h2></center>
+<Row>
+    <Col>
+        <Card>
+            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
+            <Card.Body>
+                <Card.Text>
+                <h2>Jane Doe</h2>
+                <p >CEO & Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p><Button >Contact</Button></p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </Col>
+              
+    <Col>
+        <Card>
+            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
+            <Card.Body>
+                <Card.Text>
+                <h2>Jane Doe</h2>
+                <p >CEO & Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p><Button >Contact</Button></p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </Col>
 
-  <div class="column">
-    <div class="card">
-      <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
-      <div class="container">
-        <h2>Mike Ross</h2>
-        <p class="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>mike@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="/w3images/team3.jpg" alt="John" style="width:100%">
-      <div class="container">
-        <h2>John Doe</h2>
-        <p class="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>john@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
-</div>
+    <Col>
+        <Card>
+            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
+            <Card.Body>
+                <Card.Text>
+                <h2>Jane Doe</h2>
+                <p >CEO & Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p><Button >Contact</Button></p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </Col>
+    <Col>
+        <Card>
+            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
+            <Card.Body>
+                <Card.Text>
+                <h2>Jane Doe</h2>
+                <p >CEO & Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p><Button >Contact</Button></p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </Col>
+    <Col>
+        <Card>
+            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
+            <Card.Body>
+                <Card.Text>
+                <h2>Jane Doe</h2>
+                <p >CEO & Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p><Button >Contact</Button></p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </Col>
+    </Row>
       </Container>
     </>
   );
