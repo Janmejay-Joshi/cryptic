8dc2532fcc1ec4562bd938872c216fdb0d98e208
Codefreak69 <iamvikashkumar30@gmail.com>
2021-03-21 07:00:22 +0530

    about us page

--- frontend/src/screens/AboutUs.js
+++ frontend/src/screens/AboutUs.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Container,Card, Image,Row, Col, Button } from "react-bootstrap";
+import { Container, Card, Image, Row, Col, Button } from "react-bootstrap";
 import Meta from "../components/Meta";
 import { listQuestions } from "../actions/questionActions";
 
@@ -10,87 +10,122 @@ const AboutUs = ({ match }) => {
     <>
       <Meta />
       <Container>
-          <Container>
-  <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
-  <p>Resize the browser window to see that this page is responsive by the way.</p>
-          </Container>
+        <Container>
+          <h1>About Us</h1>
+          <p>
+            We are a team of 5 members, from{" "}
+            <b>MADHAV INSTITUTE OF TECHNOLOGY AND SCIENCE</b>. We collaborated
+            to create this project{" "}
+          </p>
+        </Container>
 
-          <center><h2>Our Team</h2></center>
-<Row>
-    <Col>
-        <Card>
-            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
-            <Card.Body>
+        <center>
+          <h2>Our Team</h2>
+        </center>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Img
+                src="uploads\members\janmejay.jpeg"
+                alt="Janmejay"
+                fluid
+              />
+              <Card.Body>
                 <Card.Text>
-                <h2>Jane Doe</h2>
-                <p >CEO & Founder</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>jane@example.com</p>
-                <p><Button >Contact</Button></p>
+                  <h2>Janmejay Joshi</h2>
+                  <p>Full satck developer</p>
+                  <p>Avid LINUX enthusiast, Part-time gamer</p>
+                  <p>janmejayjoshi2002@gmail.com</p>
+                  <p>
+                    <Button href="https://www.instagram.com/janmejayjoshi/">
+                      Contact
+                    </Button>
+                  </p>
                 </Card.Text>
-            </Card.Body>
-        </Card>
-    </Col>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card>
+              <Card.Img
+                src="uploads\members\Anishka.jpeg"
+                alt="Anishka"
+                fluid
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h2>Anishka Soni</h2>
+                  <p>Team-leader & Content writer</p>
+                  <p>Interest in content writing and graphic designing</p>
+                  <p>anishkasoni2000@gmail.com</p>
+                  <p>
+                    <Button href="https://www.instagram.com/_the_ani_/">
+                      Contact
+                    </Button>
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card>
+              <Card.Img src="uploads\members\vk.jpg" alt="vikash" fluid />
+              <Card.Body>
+                <Card.Text>
+                  <h2>Vikash Kumar</h2>
+                  <p>Front end stuffs</p>
+                  <p>A Gamer, A coder and sometimes a photographer</p>
+                  <p>iamvikashkumar30@gmail.com</p>
+                  <p>
+                    <Button href="https://www.instagram.com/vikashkr30">
+                      Contact
+                    </Button>
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img src="uploads\members\MOhit.jpeg" alt="Mohit" fluid />
+              <Card.Body>
+                <Card.Text>
+                  <h2>Mohit Wankhade</h2>
+                  <p>Content writer</p>
+                  <p>Gaming is life</p>
+                  <p>wmohit301@gmail.com</p>
+                  <p>
+                    <Button href="https://www.instagram.com/mystic_luminary/">
+                      Contact
+                    </Button>
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
               
-    <Col>
-        <Card>
-            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
-            <Card.Body>
+                <Card.Img style={{height:300, width:250}}src="uploads\members\Sumit.jpeg" alt="Sumit" fluid />
+              
+              <Card.Body>
                 <Card.Text>
-                <h2>Jane Doe</h2>
-                <p >CEO & Founder</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>jane@example.com</p>
-                <p><Button >Contact</Button></p>
+                  <h2>Sumit Singh Ghuraiya</h2>
+                  <p>Front-end</p>
+                  <p>Traveller, wanderer</p>
+                  <p>sumitghuraiya@gmail.com</p>
+                  <p>
+                    <Button href="https://www.instagram.com/sumitghuraiya/">
+                      Contact
+                    </Button>
+                  </p>
                 </Card.Text>
-            </Card.Body>
-        </Card>
-    </Col>
-
-    <Col>
-        <Card>
-            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
-            <Card.Body>
-                <Card.Text>
-                <h2>Jane Doe</h2>
-                <p >CEO & Founder</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>jane@example.com</p>
-                <p><Button >Contact</Button></p>
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    </Col>
-    <Col>
-        <Card>
-            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
-            <Card.Body>
-                <Card.Text>
-                <h2>Jane Doe</h2>
-                <p >CEO & Founder</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>jane@example.com</p>
-                <p><Button >Contact</Button></p>
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    </Col>
-    <Col>
-        <Card>
-            <Card.Img src="/w3images/team1.jpg" alt="Jane" fluid/>
-            <Card.Body>
-                <Card.Text>
-                <h2>Jane Doe</h2>
-                <p >CEO & Founder</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>jane@example.com</p>
-                <p><Button >Contact</Button></p>
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    </Col>
-    </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </>
   );
